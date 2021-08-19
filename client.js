97a7cd826d0aaff30333f654ca8c230d89b13f59
Dara Akitoye <daraakitoye1@gmail.com>
2021-08-19 06:30:23 +0000

    refactored

--- client.js
+++ client.js
@@ -1,42 +1,32 @@
 const net = require('net');
-const rl = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
+const { IP, PORT } = require('./constants');
 
 //Establishes connection with the game server
 
 const connect = () => {
   const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541,
+    host: IP,
+    port: PORT,
 
   });
   //interprets incoming data as text
   conn.setEncoding('utf8');
 
   conn.on('connect', () => {
-    console.log('Succesfully connected to the game server')
+    setTimeout(() => {
+      console.log('Succesfully connected to the game server')
+    }, setTimeout(() => {
+      console.log('How to play:\nW: Moves snek up\nA: Moves snek right\nS: Moves snek down\nD: Moves snek right\nCTRL + C: quit snek')
+    }, 2000), 3000)
+
     //sends user input to game server
-    rl.question('Input username: ', (name) => {
-      if (name.length === 3) {
-        conn.write(`Name: ${name.toUpperCase()}`);
-        rl.close();
-        //refactor this block later
-      } else {
-        throw new Error('Length of Username !== 3')
-      }
-      // setInterval(() => {
-      //   conn.write('Move: up')
-      // }, 500)
-    });
-
-
+    conn.write('Name: YOU')
   });
+
   //log closing message to console after 5 seconds of inactivty
   conn.on('data', () => {
-    console.log('\nu died for idling too long silly')
+    console.log('\nu died :(')
 
   });
   return conn;
