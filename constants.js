5c44a0bdc3caf3b7786b7735849c379dcb45abf2
Dara Akitoye <daraakitoye1@gmail.com>
2021-08-19 06:24:36 +0000

    implemented constants

---
+++ constants.js
@@ -0,0 +1,17 @@
+
+const IP = '10.0.2.15'
+const PORT = 50541
+//ctrls
+const moveUp = "Move: up";
+const moveLeft = "Move: left";
+const moveDown = "Move: down";
+const moveRight = "Move: right";
+
+module.exports = {
+  IP,
+  PORT,
+  moveUp,
+  moveLeft,
+  moveDown,
+  moveRight
+}
