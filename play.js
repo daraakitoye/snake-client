bb2e45c80e011bddef1c81309acf27173c2cb170
Dara Akitoye <daraakitoye1@gmail.com>
2021-08-19 00:41:33 +0000

    moved handleUserInput oustide of setupInput

--- play.js
+++ play.js
@@ -7,19 +7,21 @@ connect();
  * Specifically, so that we can handle user input via stdin
  */
 
+const handleUserInput = ('data', (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+});
+
 const setupInput = () => {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
-  const handleUserInput = ('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-
-  });
-
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  })
 
 
   return stdin;
